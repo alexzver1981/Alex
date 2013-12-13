cf1ef0f46c7a192ccbb5389bc933766b6c0bda4a
Alexandr Zverev <alexzver1981@mail.ru>
2013-12-13 22:57:51 +0400

    work Промежуток первое активити

--- gen/alex/project001/R.java
+++ gen/alex/project001/R.java
@@ -10,6 +10,10 @@ package alex.project001;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int wait=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,38 +21,47 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_dart_weider=0x7f020000;
-        public static final int ic_fon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_yoda=0x7f020003;
-        public static final int orangr=0x7f020004;
-        public static final int rect=0x7f020005;
+        public static final int ic_back=0x7f020000;
+        public static final int ic_dart_weider=0x7f020001;
+        public static final int ic_fon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_yoda=0x7f020004;
+        public static final int orangr=0x7f020005;
+        public static final int rect=0x7f020006;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080000;
-        public static final int TextView1=0x7f080001;
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080003;
-        public static final int textView1=0x7f080002;
+        public static final int RelativeLayout1=0x7f090000;
+        public static final int TextView1=0x7f090001;
+        public static final int action_settings=0x7f09000c;
+        public static final int button1=0x7f090003;
+        public static final int imageView1=0x7f090007;
+        public static final int linearLayout1=0x7f090004;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090005;
+        public static final int textView3=0x7f090006;
+        public static final int textView4=0x7f090008;
+        public static final int textView5=0x7f090009;
+        public static final int textView6=0x7f09000a;
+        public static final int textView7=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int two=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int Text=0x7f050005;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int doclad=0x7f050003;
-        public static final int hello_world=0x7f050002;
-        public static final int nullText=0x7f050004;
+        public static final int Text=0x7f060005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int doclad=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int nullText=0x7f060004;
     }
     public static final class style {
         /** 
@@ -72,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
