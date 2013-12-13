29bfe8b07cfdfe3075297f53552373af8d6c23af
Alexandr Zverev <alexzver1981@mail.ru>
2013-12-13 20:18:35 +0400

    zxcxcvvbb

--- gen/alex/project042/R.java
+++ gen/alex/project042/R.java
@@ -25,10 +25,11 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int btCansel=0x7f080003;
         public static final int btStart=0x7f080002;
         public static final int progressBar1=0x7f080004;
+        public static final int textView1=0x7f080005;
         public static final int tv=0x7f080001;
     }
     public static final class layout {
