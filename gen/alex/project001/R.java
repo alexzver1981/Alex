6e46a550e224682571697d39bf3a49a71c98271e
Alexandr Zverev <alexzver1981@mail.ru>
2013-12-17 14:27:21 +0400

    work прикрутил стили с горем пополам

--- gen/alex/project001/R.java
+++ gen/alex/project001/R.java
@@ -26,15 +26,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_back=0x7f020000;
-        public static final int ic_dart_weider=0x7f020001;
-        public static final int ic_fon=0x7f020002;
-        public static final int ic_foto=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int ic_yoda=0x7f020005;
-        public static final int orangr=0x7f020006;
-        public static final int rect=0x7f020007;
-        public static final int rect1=0x7f020008;
+        public static final int apptheme_btn_default_disabled_focused_holo_light=0x7f020000;
+        public static final int apptheme_btn_default_disabled_holo_light=0x7f020001;
+        public static final int apptheme_btn_default_focused_holo_light=0x7f020002;
+        public static final int apptheme_btn_default_holo_light=0x7f020003;
+        public static final int apptheme_btn_default_normal_holo_light=0x7f020004;
+        public static final int apptheme_btn_default_pressed_holo_light=0x7f020005;
+        public static final int ic_back=0x7f020006;
+        public static final int ic_dart_weider=0x7f020007;
+        public static final int ic_fon=0x7f020008;
+        public static final int ic_foto=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int ic_yoda=0x7f02000b;
+        public static final int orangr=0x7f02000c;
+        public static final int rect=0x7f02000d;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090000;
